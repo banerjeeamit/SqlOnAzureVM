db833e4221f13404a056a0cd487d401a4bc0e94a
Amit Banerjee <amit.banerjee@microsoft.com>
2016-02-27 16:50:58 -0800

    Added files via upload
    Get-AllocationUnitCheck - Performs a check to see if the allocation unit size is set to 64K

---
+++ Get-AllocationUnitCheck.ps1
@@ -0,0 +1,36 @@
+﻿function global:Split-Result()
+{
+param
+(
+[parameter(ValueFromPipeline=$true,
+Mandatory=$true)]
+[Array]$MATCHRESULT
+)
+
+process
+{ 
+$ReturnData=NEW-OBJECT PSOBJECT –property @{Title=’’;Value=’’}
+$DATA=$Matchresult[0].tostring().split(":")
+$ReturnData.Title=$Data[0].trim()
+$ReturnData.Value=$Data[1].trim()
+Return $ReturnData
+}
+}
+
+$LogicalDisks = Get-WmiObject -Query "select * from Win32_LogicalDisk Where MediaType = 12" | Select Name, MediaType, FileSystem, Size
+
+foreach ($disk in $LogicalDisks)
+{
+    $Drive = $disk.Name + "\"
+    $RESULTS=(fsutil fsinfo ntfsinfo $Drive) 
+    $AllocSize = $Results | Split-Result | Select-Object Title,Value | Where-Object {$_.Title -eq "Bytes Per Cluster"}
+    if ($AllocSize.Value -eq 65536)
+    {
+        Write-Host "Allocation size for " $Drive " = " $AllocSize.Value " bytes" -ForegroundColor Green
+    }
+    else 
+    {
+        Write-Host "Allocation size for " $Drive " = " $AllocSize.Value " bytes (Recommendation is 64K)" -ForegroundColor Red
+    }
+}
+
