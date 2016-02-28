44dff9e697fc7c8eded6c895cc372e293dd00565
Amit Banerjee <amit.banerjee@microsoft.com>
2016-02-27 16:52:09 -0800

    Added files via upload
    Checks to see if AUTO CLOSE or AUTO SHRINK is enabled or any database

---
+++ Get-DBProperties.ps1
@@ -0,0 +1,31 @@
+﻿Param(
+  [Parameter(Mandatory=$True)]
+   [string]$sqlserver
+)
+
+
+$RulePass = 1
+$sqlquery = "select name, is_auto_shrink_on, is_auto_close_on from sys.databases"
+
+# Execute a query against the SQL Server instance
+$dbs = Invoke-Sqlcmd -ServerInstance $sqlserver -Query $sqlquery
+
+# Loop through the database properties
+foreach ($db in $dbs)
+{
+    if ($db.is_auto_shrink_on -eq $true)
+    {
+        Write-Host "[ERR] Database" $db.name "has Auto Shrink turned on" -ForegroundColor Red
+        $RulePass = 0
+    }
+    if ($db.is_auto_close_on -eq $true)
+    {
+        Write-Host "[ERR] Database" $db.name "has Auto Close turned on" -ForegroundColor Red
+        $RulePass = 0
+    }
+}
+
+if ($RulePass -eq 1)
+{
+    Write-Host "[INFO] No databases found with Auto Close and Auto Shrink turned on" -ForegroundColor Green
+}
