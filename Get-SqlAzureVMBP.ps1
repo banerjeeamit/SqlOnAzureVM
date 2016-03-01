af5f0f014d8e763c9b2a7d6c2d9b0705727c5384
Amit Banerjee <amit.banerjee@microsoft.com>
2016-03-01 02:09:44 -0800

    Added files via upload

---
+++ Get-SqlAzureVMBP.ps1
@@ -0,0 +1,18 @@
+﻿cls
+$VMName = ""
+$RGName = ""
+$sqlserver = ""
+# Find out the temporary drive as it is needed for other checks
+$TempDrive = & '.\Temporary Drive.ps1'
+# Check if the correct VM size is being used
+Write-Host "***** Check for storage best practices"
+&'.\Get-AllocationUnitCheck.ps1' 
+&'.\Get-StorageAccountBP.ps1' $VMName $RGName
+&'.\Get-VMSize.ps1' $VMName $RGName
+&'.\Get-IFI.ps1'
+&'.\Get-LPIM.ps1' $sqlserver
+Write-Host "***** Check for database properties"
+&'.\Get-OSFilesDB.ps1' $sqlserver
+&'.\Get-DBProperties.ps1' $sqlserver
+&'.\Get-FilesOnTemp.ps1' $sqlserver $TempDrive 
+&'.\Get-Backups.ps1' $sqlserver 
